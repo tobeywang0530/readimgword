fcdb60ff437ad5dbee95ad7d37306d713b3b1a05
tobey_cathaysite <hebeplkj@gmail.com>
2019-03-28 10:08:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -73,8 +73,8 @@ def scan():
     full_text=''
     if(filename!=''):
         try:
-            ocr.pytesseract.tesseract_cmd =OCR_FOLDER+ r'/tesseract.exe'
-            tessdata_dir_config = '--tessdata-dir "'+OCR_FOLDER+'\\tessdata"'
+            ocr.pytesseract.tesseract_cmd =r'Tesseract-OCR/tesseract.exe' 
+            tessdata_dir_config = '--tessdata-dir "Tesseract-OCR\\tessdata"'
             #get file
             #full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             full_filename=filename
